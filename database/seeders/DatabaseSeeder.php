78948556644c87115315054f0ace077118cca021
TheKiryuKha <kirillzuyeu@gmai.com>
2025-11-06 19:50:29 +0300

    feat: simple seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Cache;
 
@@ -12,5 +13,21 @@ final class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Cache::flush();
+
+        Product::create([
+            'title' => 'Дикий Мёд',
+            'description' => '(Очень крутое описание)',
+            'price' => '2000'
+        ])->addMedia(
+            public_path('assets/images/honey.jpg')
+        )->preservingOriginal()->toMediaCollection('image');
+
+        Product::create([
+            'title' => 'Яйца Валерия',
+            'description' => '(Очень крутое описание)',
+            'price' => '2000'
+        ])->addMedia(
+            public_path('assets/images/eggs.jpg')
+        )->preservingOriginal()->toMediaCollection('image');
     }
 }
