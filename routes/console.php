0862e120d15e86f19a46e8ddc682e4915147cffb
TheKiryuKha <kirillzuyeu@gmai.com>
2025-11-10 18:03:17 +0300

    fix: dayly checking visas

--- routes/console.php
+++ routes/console.php
@@ -2,4 +2,11 @@
 
 declare(strict_types=1);
 
-//
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::daily()
+    ->onOneServer()
+    ->timezone('Asia/Manila')
+    ->group(function () {
+        Schedule::command('check-visas');
+    });
